73eb80c42a7458cd27a1100a4431242a49bde0a0
Shazid <Milgate#313>
2025-02-10 20:29:53 +0600

    layout js made with nativewind

--- app/(tab)/_layout.tsx
+++ app/(tab)/_layout.tsx
@@ -2,53 +2,54 @@ import { Tabs } from "expo-router";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
-import { StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { View } from "react-native";
 
 export default function TabLayout() {
   return (
-
-      <Tabs
+    <Tabs
       screenOptions={{
         headerShown: false,
-        tabBarStyle: styles.tabBar,
+        tabBarStyle: {
+          backgroundColor: "#fff",
+          borderTopWidth: 1,
+          borderTopColor: "#ddd",
+          height: 50,
+        },
         tabBarShowLabel: false,
       }}
     >
       <Tabs.Screen
         name="newsfeed"
         options={{
-          tabBarIcon: ({ focused }) => <FontAwesome5 name="newspaper" size={24} color={focused ? "#007BFF" : "#aaa"} />,
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome5 name="newspaper" size={24} color={focused ? "#007BFF" : "#aaa"} />
+          ),
         }}
       />
       <Tabs.Screen
         name="batch"
         options={{
-          tabBarIcon: ({ focused }) => <MaterialCommunityIcons name="account-group" size={28} color={focused ? "#007BFF" : "#aaa"} />,
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons name="account-group" size={28} color={focused ? "#007BFF" : "#aaa"} />
+          ),
         }}
       />
       <Tabs.Screen
         name="chat"
         options={{
-          tabBarIcon: ({ focused }) => <Ionicons name="chatbubble-ellipses" size={24} color={focused ? "#007BFF" : "#aaa"} />,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons name="chatbubble-ellipses" size={24} color={focused ? "#007BFF" : "#aaa"} />
+          ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
-          tabBarIcon: ({ focused }) => <FontAwesome5 name="user" size={24} color={focused ? "#007BFF" : "#aaa"} />,
+          tabBarIcon: ({ focused }) => (
+            <FontAwesome5 name="user" size={24} color={focused ? "#007BFF" : "#aaa"} />
+          ),
         }}
       />
     </Tabs>
-    
   );
 }
-
-const styles = StyleSheet.create({
-  tabBar: {
-    backgroundColor: "#fff",
-    borderTopWidth: 1,
-    borderTopColor: "#ddd",
-    height: 50,
-  },
-});
